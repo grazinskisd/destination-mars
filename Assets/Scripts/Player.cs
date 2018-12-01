665639dafaa52db7556c1d5a48e63b084f4fe75c
Dalius Grazinskis <grazinskisd@gmail.com>
2018-12-01 13:58:15 +0100

    Working on MVP

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public delegate void PlayerEventHandler(Player sender);
+
+public class Player : MonoBehaviour
+{
+    public float MoveSpeed;
+    public float Water;
+    public float WaterUsage;
+    public Slider WaterMeter;
+
+    private Transform _transform;
+    private float _timePassed;
+    private float _startWater;
+
+    private void Awake()
+    {
+        _transform = transform;
+        _timePassed = 0;
+        _startWater = Water;
+    }
+
+    void Start () {
+
+	}
+
+	void Update () {
+        if (Horizontal != 0 || Vertical != 0)
+        {
+            _transform.position += Time.deltaTime * MoveSpeed * (Vector3.right * Horizontal + Vector3.forward * Vertical);
+            _timePassed += Time.deltaTime;
+
+            var usedWater = WaterUsage * _timePassed;
+            WaterMeter.value = (_startWater - usedWater) / _startWater;
+
+            if(usedWater >= _startWater)
+            {
+                Die();
+            }
+        }
+	}
+
+    private void Die()
+    {
+        throw new NotImplementedException();
+    }
+
+    private float Horizontal
+    {
+        get { return Input.GetAxis("Horizontal"); }
+    }
+
+    private float Vertical
+    {
+        get { return Input.GetAxis("Vertical"); }
+    }
+}
