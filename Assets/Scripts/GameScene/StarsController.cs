17bc3a47b2fe6e167fb88766cca9feae79e9d015
Dalius Grazinskis <grazinskisd@gmail.com>
2018-12-02 18:18:40 +0100

    Stary sky

---
+++ Assets/Scripts/GameScene/StarsController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Ldjam43
+{
+    public class StarsController: MonoBehaviour
+    {
+        public Transform Follow;
+        public float Lag;
+
+        private Vector3 _lastPosition;
+
+        private void Update()
+        {
+            if(_lastPosition != Follow.position)
+            {
+                var diff = Follow.position - _lastPosition;
+                transform.position += diff * Lag;
+                _lastPosition = Follow.position;
+            }
+        }
+    }
+}
